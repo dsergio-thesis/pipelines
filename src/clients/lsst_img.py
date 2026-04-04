26b38099302a190130034e52a77a224b559b15b3
David Sergio <davidsergio52@gmail.com>
2026-04-04 01:50:49 -0700

    client

--- src/clients/lsst_img.py
+++ src/clients/lsst_img.py
@@ -10,88 +10,66 @@ def main():
     max_records = config.max_records
     print("Configuration loaded successfully.")
 
-    # dataset_cart_cutouts_morph = FITS_Image_Morphometry_Photometry_Dataset(
-            # dataset_dir=os.path.join(dataset_dir, dataset_name),
-            # labels_init_file=label_def_file,
-            # N_bands=5, 
-            # N_morphometric_features=4,
-            # N_photometric_features=4,
-            # )
 
-    dataset_cart_cutouts_morph_b = FITS_Image_Morphometry_Photometry_Dataset(
+    dataset = FITS_Image_Morphometry_Photometry_Dataset(
             dataset_dir=os.path.join(dataset_dir, dataset_name),
             labels_init_file=label_def_file,
             )
 
-    catalog = LSSTNodeCatalog(
+    lsst_catalog = LSSTNodeCatalog(
             parameters={
                 "max_records": max_records,
                 "query_coords": pipeline_metadata.get("query_coords", None),
                 "query_radius": pipeline_metadata.get("query_radius", None),})
 
-    match = LSSTNodeMatchToHST(
+    # we usually want the whole catalog for matching. adjust just for testing this
+    hst_max_records = 300000
+    hst_catalog = HSTNodeCatalog(parameters={"max_records": hst_max_records}) 
+    hst_clean = HSTNodeClean(parameters={"max_records": hst_max_records},
+                     parents=[hst_catalog.node_id])
+    hst_select_clean = HSTNodeSelect(parameters={"max_records": hst_max_records},
+                     parents=[hst_clean.node_id])
+
+    lsst_hst_match = LSSTNodeMatchToHST(
             parameters={
                 "max_records": max_records,},
-            parents=[catalog.node_id],
+            parents=[lsst_catalog.node_id, hst_select_clean.node_id],
             )
 
-    preprocess = LSSTNodePreprocess(
+    lsst_hst_preprocess = LSSTNodePreprocess(
             parameters={
-                "dataset": dataset_cart_cutouts_morph_b.to_dict()
-                },
-            parents=[match.node_id],
+                "max_records": max_records,},
+            parents=[lsst_hst_match.node_id],
+            )
+    lsst_hst_data = LSSTNodePhotoDataset(
+            parameters={
+                "dataset": dataset.to_dict()},
+            parents=[lsst_hst_preprocess.node_id],
             )
 
-    fetch = LSSTNodeButlerFetch(
+    lsst_hst_fetch = LSSTNodeButlerFetch(
             parameters={
-                "dataset": dataset_cart_cutouts_morph_b.to_dict()
+                "dataset": dataset.to_dict()
                 },
-            parents=[preprocess.node_id],
+            parents=[lsst_hst_data.node_id],
             )
 
     dag = PipelineDAG()
 
-    dag.add_node(catalog)
-    dag.add_node(match)
-    dag.add_node(preprocess)
-    dag.add_node(fetch)
+    dag.add_node(hst_catalog)
+    dag.add_node(hst_clean)
+    dag.add_node(hst_select_clean)
 
-    dag.run_from_node(catalog.node_id)
+    dag.add_node(lsst_catalog)
+    dag.add_node(lsst_hst_match)
+    dag.add_node(lsst_hst_preprocess)
+    dag.add_node(lsst_hst_data)
 
-    dag.to_yaml("_pipelines/lsst_img_pipeline.yaml")
+    dag.run_from_node(lsst_catalog.node_id)
 
-    # pipelines = [
-            # PipelineClassification(
-                # name=pipeline_name,
-                # metadata=pipeline_metadata,
-                # max_records=max_records,
-                # dataset=dataset_cart_cutouts_morph,
-                # minor_version=None,
-                # ),
-            # PipelineClassification(
-                # name=pipeline_name,
-                # metadata=pipeline_metadata,
-                # max_records=max_records,
-                # dataset=dataset_cart_cutouts_morph_b,
-                # minor_version=None,
-                # ),
-            # ]
+    dag.to_graphviz()
 
-    # pipelines[0].add_stages([ # Soda
-        # StageCatalogLSST(),
-        # StageMatchLSSTtoHST(),
-        # StagePreprocessLSST(),
-        # StageFetchLSSTSoda(),
-        # ])
-
-    # pipelines[1].add_stages([ # Butler
-        # StageCatalogLSST(),
-        # StageMatchLSSTtoHST(),
-        # StagePreprocessLSST(),
-        # StageButlerFetchLSST(),
-        # ])
-
-    # pipelines[1].run_pipeline()
+    dag.to_yaml("_pipelines/lsst_hst_img.yaml")
 
 if __name__ == "__main__":
     main()
