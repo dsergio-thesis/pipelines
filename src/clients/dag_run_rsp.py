9624ffb97e1aeda75d4dded2c85dd6beac99a395
David Sergio <davidsergio52@gmail.com>
2026-05-09 22:09:19 -0700

    run rsp

---
+++ src/clients/dag_run_rsp.py
@@ -0,0 +1,20 @@
+
+from clients._rsp import *
+
+def main():
+
+    config = client_config()
+    pipeline_name = config.pipeline_name
+
+    dag = PipelineDAG(label=pipeline_name)
+    if not dag.is_initialized():
+        print("No pipelines found.")
+        PipelineDAG.usage()
+        return
+
+    dag.run()
+    dag.to_yaml()
+    dag.to_graphviz()
+
+if __name__ == "__main__":
+    main()
