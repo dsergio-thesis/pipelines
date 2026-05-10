cfd48867d371f060878356e087eb933cb64198ac
David Sergio <davidsergio52@gmail.com>
2026-05-09 21:33:53 -0700

    test

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@ make clean
 rad init
 rad node -c
 rad node -ct import
-rad node -i catalogs/hst/hst.fits
+rad node -i catalogs/collections/lsst-hst/hst/hst.fits
 rad node -p max_records 1000
-rad node -p script catalogs/hst/scripts/import.py
+rad node -p script catalogs/collections/lsst-hst/hst/scripts/import.py
 rad node -ct script
-rad node -p script catalogs/hst/scripts/clean.py
+rad node -p script catalogs/collections/lsst-hst/hst/scripts/clean.py
 rad node -ct export
 rad node -ct eda
