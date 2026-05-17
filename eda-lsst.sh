022d2e2324e18da65200b2a00b45a67b8d8b4162
David Sergio <davidsergio52@gmail.com>
2026-05-17 20:28:29 +0000

    lsst

--- eda-lsst.sh
+++ eda-lsst.sh
@@ -4,7 +4,7 @@ rad init-rsp
 
 rad node-rsp -c -l "Exploratory analysis pipeline on the LSST DP-1 catalog"
 
-rad node-rsp -ct tap -l "Query TAP service for LSST DP-1 catalog" --target extended_chandra_deep_field_south_ecdfs -m 300 -d d1
+rad node-rsp -ct tap -l "Query TAP service for LSST DP-1 catalog" --target extended_chandra_deep_field_south_ecdfs -m 100000 -d d1
 rad node-rsp -p script catalogs/collections/lsst-hst/lsst/scripts/query.py
 
 rad node-rsp -ct script -l "Clean catalog"
@@ -20,6 +20,4 @@ rad node-rsp -p title "Exploratory analysis of the LSST DP-1 catalog"
 
 rad run-rsp
 
-cp "_pipelines/$(rad id)/dag.svg" ~/thesis-org/reports/paper/report/figures/dag_lsst_eda/
-cp "_pipelines/$(rad id)/dag.yaml" ~/thesis-org/reports/paper/report/code/dag_lsst_eda/
 
