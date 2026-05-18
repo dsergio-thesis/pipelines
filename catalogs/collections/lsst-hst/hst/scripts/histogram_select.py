6f500d9af7dfced46076cb96c38da0e39544f049
David Sergio <davidsergio52@gmail.com>
2026-05-18 00:56:42 -0700

    label

--- catalogs/collections/lsst-hst/hst/scripts/histogram_select.py
+++ catalogs/collections/lsst-hst/hst/scripts/histogram_select.py
@@ -9,6 +9,6 @@
 
 keys = list(columns.keys())
 for col in keys:
-    if col not in ['z_best', 'lmass', 'lssfr', 'Av', 'beta']:
+    if col not in ['z_best', 'lmass', 'lssfr', 'Av', 'beta', 'label']:
         columns.pop(col, None)
         df.pop(col)
